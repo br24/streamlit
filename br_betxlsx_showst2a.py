9197bdc3b1b833c92cff9225b88ae5bea1b9e243
br24 <38184167+br24@users.noreply.github.com>
2022-01-27 16:24:04 +0900

    update

--- br_betxlsx_showst2a.py
+++ br_betxlsx_showst2a.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 #import matplotlib
 #matplotlib.use("Agg")
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #from matplotlib.backends.backend_agg import RendererAgg
 #from matplotlib.figure import Figure
 #import plotly.graph_objects as go
